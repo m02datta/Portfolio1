a35cc6d90eabed53d7869dba98f6a03d158dff55
m02datta <m02659501@gmail.com>
2026-09-05 17:13:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+
+  base: "/Portfolio1/",
+
   build: {
     outDir: "dist",
     sourcemap: false,
-  chunkSizeWarningLimit: 1200,
-  rollupOptions: {
+    chunkSizeWarningLimit: 1200,
+
+    rollupOptions: {
       output: {
         manualChunks: {
           vendor: ["react", "react-dom"],
